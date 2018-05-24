c3ccb84bcc3179009073974322ee96dbc07e5c5c
Albilaga L. Pradana <albilaga.lp@gmail.com>
2018-05-25 00:06:14 +0700

    initial commit

---
+++ tes/AppDelegate.cs
@@ -0,0 +1,25 @@
+﻿using AppKit;
+using FFImageLoading;
+using Foundation;
+
+namespace tes
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : NSApplicationDelegate
+    {
+        public AppDelegate()
+        {
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // Insert code here to initialize your application
+            ImageService.Instance.Initialize();
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
---
+++ tes/ViewController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using AppKit;
+using FFImageLoading;
+using Foundation;
+
+namespace tes
+{
+    public partial class ViewController : NSViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+           //Set directly from resource is working
+            var imageView = new NSImageView
+            {
+                Frame = this.View.Bounds,
+                Image=NSImage.ImageNamed("food.jpg")
+            };
+            this.View.AddSubview(imageView);
+            //Using Image Service is not working to load pictures into NSImageView
+            ImageService.Instance.LoadUrl("https://blog.traveloka.com/wp-content/uploads/2017/09/kampung_warna_warni_jodipan_malang_bs1.jpg").LoadingPlaceholder("food.jpg",FFImageLoading.Work.ImageSource.ApplicationBundle).Into(imageView);
+
+            //Set directly from url is working
+            //var imageView = new NSImageView
+            //{
+            //    Frame = this.View.Bounds,
+            //    Image = new NSImage(new NSUrl("https://blog.traveloka.com/wp-content/uploads/2017/09/kampung_warna_warni_jodipan_malang_bs1.jpg"))
+            //};
+            //this.View.AddSubview(imageView);
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
